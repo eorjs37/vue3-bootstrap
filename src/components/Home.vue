0571f54368b75fde7ba4ef1dafb06f8d9ae872fa
maxgunchoi <maxgunchoi@carrotglobal.com>
2022-03-21 08:44:45 +0900

    Blog 화면 추가

---
+++ src/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <Blog/>
+</template>
+
+<script>
+import Blog from '/@/components/Blog.vue';
+export default {
+    components:{
+        Blog
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,9 +1,13 @@
+import Home from '/@components/Home.vue';
 import Profile from '/@components/Profile.vue';
+import Application from '/@components/Application.vue';
 import NotFound from '/@components/NotFound.vue';
 
 const routes = [
-    { path: '/', redirect: '/profile' },
-    { path: '/profile', name: 'profile', component: Profile, alias: '/home' },
+    { path: '/', redirect: '/home' },
+    { path: '/home', name: 'home', component: Home},
+    { path: '/profile', name: 'profile', component: Profile},
+    { path: '/application', name: 'application', component: Application},
     { path: '/:pathMatch(.*)*', component: NotFound },
 ]
 
