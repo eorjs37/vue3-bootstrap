86027d44dc844f16b2c46826290c387306f9f1ce
maxgunchoi <maxgunchoi@carrotglobal.com>
2022-03-22 08:47:28 +0900

    블로그 진행중 및 아카이브 분석필요

--- src/components/Blog.vue
+++ src/components/Blog.vue
@@ -47,124 +47,17 @@
             From the Firehose
         </h3>
 
-        <article class="blog-post">
-            <h2 class="blog-post-title">Sample blog post</h2>
-            <p class="blog-post-meta">January 1, 2021 by <a href="#">Mark</a></p>
-
-            <p>This blog post shows a few different types of content that’s supported and styled with Bootstrap. Basic typography, lists, tables, images, code, and more are all supported as expected.</p>
-            <hr>
-            <p>This is some additional paragraph placeholder content. It has been written to fill the available space and show how a longer snippet of text affects the surrounding content. We'll repeat it often to keep the demonstration flowing, so be on the lookout for this exact same string of text.</p>
-            <h2>Blockquotes</h2>
-            <p>This is an example blockquote in action:</p>
-            <blockquote class="blockquote">
-            <p>Quoted text goes here.</p>
-            </blockquote>
-            <p>This is some additional paragraph placeholder content. It has been written to fill the available space and show how a longer snippet of text affects the surrounding content. We'll repeat it often to keep the demonstration flowing, so be on the lookout for this exact same string of text.</p>
-            <h3>Example lists</h3>
-            <p>This is some additional paragraph placeholder content. It's a slightly shorter version of the other highly repetitive body text used throughout. This is an example unordered list:</p>
-            <ul>
-            <li>First list item</li>
-            <li>Second list item with a longer description</li>
-            <li>Third list item to close it out</li>
-            </ul>
-            <p>And this is an ordered list:</p>
-            <ol>
-            <li>First list item</li>
-            <li>Second list item with a longer description</li>
-            <li>Third list item to close it out</li>
-            </ol>
-            <p>And this is a definiton list:</p>
-            <dl>
-            <dt>HyperText Markup Language (HTML)</dt>
-            <dd>The language used to describe and define the content of a Web page</dd>
-            <dt>Cascading Style Sheets (CSS)</dt>
-            <dd>Used to describe the appearance of Web content</dd>
-            <dt>JavaScript (JS)</dt>
-            <dd>The programming language used to build advanced Web sites and applications</dd>
-            </dl>
-            <h2>Inline HTML elements</h2>
-            <p>HTML defines a long list of available inline tags, a complete list of which can be found on the <a href="https://developer.mozilla.org/en-US/docs/Web/HTML/Element">Mozilla Developer Network</a>.</p>
-            <ul>
-            <li><strong>To bold text</strong>, use <code class="language-plaintext highlighter-rouge">&lt;strong&gt;</code>.</li>
-            <li><em>To italicize text</em>, use <code class="language-plaintext highlighter-rouge">&lt;em&gt;</code>.</li>
-            <li>Abbreviations, like <abbr title="HyperText Markup Langage">HTML</abbr> should use <code class="language-plaintext highlighter-rouge">&lt;abbr&gt;</code>, with an optional <code class="language-plaintext highlighter-rouge">title</code> attribute for the full phrase.</li>
-            <li>Citations, like <cite>— Mark Otto</cite>, should use <code class="language-plaintext highlighter-rouge">&lt;cite&gt;</code>.</li>
-            <li><del>Deleted</del> text should use <code class="language-plaintext highlighter-rouge">&lt;del&gt;</code> and <ins>inserted</ins> text should use <code class="language-plaintext highlighter-rouge">&lt;ins&gt;</code>.</li>
-            <li>Superscript <sup>text</sup> uses <code class="language-plaintext highlighter-rouge">&lt;sup&gt;</code> and subscript <sub>text</sub> uses <code class="language-plaintext highlighter-rouge">&lt;sub&gt;</code>.</li>
-            </ul>
-            <p>Most of these elements are styled by browsers with few modifications on our part.</p>
-            <h2>Heading</h2>
-            <p>This is some additional paragraph placeholder content. It has been written to fill the available space and show how a longer snippet of text affects the surrounding content. We'll repeat it often to keep the demonstration flowing, so be on the lookout for this exact same string of text.</p>
-            <h3>Sub-heading</h3>
-            <p>This is some additional paragraph placeholder content. It has been written to fill the available space and show how a longer snippet of text affects the surrounding content. We'll repeat it often to keep the demonstration flowing, so be on the lookout for this exact same string of text.</p>
-            <pre><code>Example code block</code></pre>
-            <p>This is some additional paragraph placeholder content. It's a slightly shorter version of the other highly repetitive body text used throughout.</p>
+        <article class="blog-post" v-for="article in posts" :key="article.id">
+          <h2 class="blog-post-title">{{ article.title }}</h2>
+          <p class="blog-post-meta">{{ article.date }}</p>
+          <div v-html="article.post"></div>
         </article>
 
-        <article class="blog-post">
-            <h2 class="blog-post-title">Another blog post</h2>
-            <p class="blog-post-meta">December 23, 2020 by <a href="#">Jacob</a></p>
-
-            <p>This is some additional paragraph placeholder content. It has been written to fill the available space and show how a longer snippet of text affects the surrounding content. We'll repeat it often to keep the demonstration flowing, so be on the lookout for this exact same string of text.</p>
-            <blockquote>
-            <p>Longer quote goes here, maybe with some <strong>emphasized text</strong> in the middle of it.</p>
-            </blockquote>
-            <p>This is some additional paragraph placeholder content. It has been written to fill the available space and show how a longer snippet of text affects the surrounding content. We'll repeat it often to keep the demonstration flowing, so be on the lookout for this exact same string of text.</p>
-            <h3>Example table</h3>
-            <p>And don't forget about tables in these posts:</p>
-            <table class="table">
-            <thead>
-                <tr>
-                <th>Name</th>
-                <th>Upvotes</th>
-                <th>Downvotes</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                <td>Alice</td>
-                <td>10</td>
-                <td>11</td>
-                </tr>
-                <tr>
-                <td>Bob</td>
-                <td>4</td>
-                <td>3</td>
-                </tr>
-                <tr>
-                <td>Charlie</td>
-                <td>7</td>
-                <td>9</td>
-                </tr>
-            </tbody>
-            <tfoot>
-                <tr>
-                <td>Totals</td>
-                <td>21</td>
-                <td>23</td>
-                </tr>
-            </tfoot>
-            </table>
-
-            <p>This is some additional paragraph placeholder content. It's a slightly shorter version of the other highly repetitive body text used throughout.</p>
-        </article>
-
-        <article class="blog-post">
-            <h2 class="blog-post-title">New feature</h2>
-            <p class="blog-post-meta">December 14, 2020 by <a href="#">Chris</a></p>
-
-            <p>This is some additional paragraph placeholder content. It has been written to fill the available space and show how a longer snippet of text affects the surrounding content. We'll repeat it often to keep the demonstration flowing, so be on the lookout for this exact same string of text.</p>
-            <ul>
-            <li>First list item</li>
-            <li>Second list item with a longer description</li>
-            <li>Third list item to close it out</li>
-            </ul>
-            <p>This is some additional paragraph placeholder content. It's a slightly shorter version of the other highly repetitive body text used throughout.</p>
-        </article>
 
         <nav class="blog-pagination" aria-label="Pagination">
-            <a class="btn btn-outline-primary" href="#">Older</a>
-            <a class="btn btn-outline-secondary disabled" href="#" tabindex="-1" aria-disabled="true">Newer</a>
+            <a href="#" class="btn" @click="page--" :class="[page == 1 ? 'btn-outline-secondary disabled' : 'btn-outline-primary']">Older</a>
+            {{ page }}
+            <a href="#" class="btn" @click="page++" :class="[page == total_pages ? 'btn-outline-secondary disabled' : 'btn-outline-primary']">Newer</a>
         </nav>
 
         </div>
@@ -177,21 +70,24 @@
             </div>
 
             <div class="p-4">
-            <h4 class="fst-italic">Archives</h4>
-            <ol class="list-unstyled mb-0">
-                <li><a href="#">March 2021</a></li>
-                <li><a href="#">February 2021</a></li>
-                <li><a href="#">January 2021</a></li>
-                <li><a href="#">December 2020</a></li>
-                <li><a href="#">November 2020</a></li>
-                <li><a href="#">October 2020</a></li>
-                <li><a href="#">September 2020</a></li>
-                <li><a href="#">August 2020</a></li>
-                <li><a href="#">July 2020</a></li>
-                <li><a href="#">June 2020</a></li>
-                <li><a href="#">May 2020</a></li>
-                <li><a href="#">April 2020</a></li>
-            </ol>
+                <h4 class="fst-italic">Archives</h4>
+                <ol class="list-unstyled mb-0">
+                    <li v-for="(archive, index) in archives" :key="index">
+                        <a href="#" data-toggle="collapse" :data-target="'#archive-'+index">
+                            {{ archive.key }}
+                        </a>
+                        <div class="collapse" :id="'archive-'+index">
+                            <ol class="list-unstyled ms-3">
+                                <li v-for="post in archive.posts" :key="post.id">
+                                    {{ post.date.substring(0,10) + ': '}}
+                                    <a :href="'#article-'+post.id" @click="onArchive($event,post.id)">
+                                        {{ post.title.substring(0,20) }}
+                                    </a>
+                                </li>
+                            </ol>
+                        </div>
+                    </li>
+                </ol>
             </div>
 
             <div class="p-4">
@@ -209,8 +105,63 @@
 </template>
 
 <script>
+import { reactive, onMounted, ref, computed } from "vue";
+import useAxios from '/@app_modules/axios.js';
 export default {
+    setup(){
+        const { axiosGet } = useAxios();
+        const posts = reactive([]);
+        onMounted(() =>{
+            axiosGet('/db/blog', onSuccess)
+        });
 
+        const onSuccess  = (data) =>{
+            Object.assign(posts, data.data);
+            total_pages.value = posts.length;
+
+
+            const temp_group = posts.reduce((accumlator,currentValue) =>{
+                ;(accumlator[currentValue['date'].substring(0,7)] = (accumlator[currentValue['date'].substring(0,7)]) || []).push(currentValue) 
+                return accumlator
+            },{});
+
+            Object.assign(
+                archives,
+                Object.keys(temp_group).map((key) =>({
+                    key:key,
+                    posts: temp_group[key]
+                }))
+            )
+        }
+
+        //pagination
+        const rows = ref(5);
+        const total_rows = ref(0);
+        const page = ref(1);
+        const total_pages = computed(() =>{
+            return Math.ceil(total_rows.value / rows.value);
+        });
+        const sliced_posts = computed(() =>{
+            return posts.slice((page.value-1)*rows.value, page.value* rows.value);
+        })
+
+
+        //archives
+        const archives = reactive([]);
+        const onArchive = (evt,id) =>{
+            const index = posts.findIndex((post) => post.id === id) +1;
+            page.value =  Math.ceil(index/rows.value);
+        }
+
+        return{
+            sliced_posts,
+            posts,
+            page,
+            total_pages,
+            archives,
+            onArchive
+        }
+    },
 }
 </script>
 
